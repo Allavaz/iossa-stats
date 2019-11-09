240d593be1189dc25f72e657678488e5fc4c56b6
fetobanana <22361341+fetobanana@users.noreply.github.com>
2019-11-09 06:08:34 -0300

    Add LMT0 and DDHT0 logos to getTournamentIcon

--- frontend/src/Utils.js
+++ frontend/src/Utils.js
@@ -19,6 +19,12 @@ exports.getTournamentIcon = function(tournament) {
 		return "tournaments/cg.png"
 	} else if (tournament.includes("Recopa Master T0")) {
 		return "tournaments/recopamastert0.png"
+	} else if (tournament.includes("Liga Master T0")) {
+		return "tournaments/ligamaster.png"
+	} else if (tournament.includes("Division de Honor T0")) {
+		return "tournaments/ddh.png"
+	} else if (tournament.includes("Recopa Master T0")) {
+		return "tournaments/recopamastert0.png"
 	} else {
 		return "tournaments/none.png"
 	}
