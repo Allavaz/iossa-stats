0259cdc7fffb75beb2686a94a4e63ecd07410c3e
David Zavalla <davidzavalla@gmail.com>
2020-02-26 01:38:09 -0300

    Add alt

--- frontend/src/Components/PlayerCard.js
+++ frontend/src/Components/PlayerCard.js
@@ -10,7 +10,7 @@ export default function PlayerCard(props) {
 			<p style={{color: 'rgb(90,90,90'}}>{props.steaminfo.data.personaname}</p>
 			<div className='teamlogo'><img style={{marginLeft: '0px'}} height='16px' src={`/clubs/${Teams[props.data[0].team].toLowerCase()}.png`} alt={props.data[0].name}></img> <div id='fullteamname' style={{color: 'rgb(90,90,90'}}>{props.data[0].team}</div></div>
 			<div className='profilepicture'>
-				<img src={props.steaminfo.data.avatarfull}></img>
+				<img src={props.steaminfo.data.avatarfull} alt={props.data[0].name}></img>
 			</div>
 			<Radar data={props.data2}></Radar>
 		</div>
