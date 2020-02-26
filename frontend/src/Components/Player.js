c1fa7087c12f379e95e88de12d4c8525e078764c
David Zavalla <davidzavalla@gmail.com>
2020-02-26 02:23:06 -0300

    Remove old steamid code

--- frontend/src/Components/Player.js
+++ frontend/src/Components/Player.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import PlayerCard from './PlayerCard';
 import { api } from '../api';
-import SteamID from 'steamid'
 
 export default class Player extends Component {
     state = {
@@ -21,10 +20,8 @@ export default class Player extends Component {
     componentDidMount() {
         axios.get(api + 'player/' + this.props.match.params.id + '/all').then(res => {
             this.setState({data: res.data});
-            let sid = new SteamID(this.props.match.params.id);
-            let sid64 = sid.getSteamID64();
             axios.get(api + 'playerlast15/' + this.props.match.params.id + '/all').then(res => {
-                axios.get(api + 'getsteaminfo/' + sid64).then(res => {
+                axios.get(api + 'getsteaminfo/' + this.props.match.params.id).then(res => {
                     this.setState({steaminfo: res, isLoading: false})
                 })
                 this.setState({data2: res.data});
