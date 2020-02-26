b77eb41c3b9b01a7e40513d6d6e25edb8342ccb6
David Zavalla <davidzavalla@gmail.com>
2020-02-26 02:21:27 -0300

    Move steamid module to backend

--- backend/steam.js
+++ backend/steam.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
 const steamapi = require('./steamapi');
+const SteamID = require('steamid');
 
-exports.getSteamInfo = async function(id64, res) {
-    let info = await axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamapi.steamapi}&steamids=${id64}`);
+exports.getSteamInfo = async function(id, res) {
+    let sid = new SteamID(id);
+    let sid64 = sid.getSteamID64();
+    let info = await axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamapi.steamapi}&steamids=${sid64}`);
     res.json(info.data.response.players[0]);
 }
