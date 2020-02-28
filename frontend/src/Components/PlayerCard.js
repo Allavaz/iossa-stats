26c12bcc0795856ad4c15e6b7b74b8e7175ecd74
David Zavalla <davidzavalla@gmail.com>
2020-02-28 01:17:33 -0300

    Obsessing over tiny details no one cares about

--- frontend/src/Components/PlayerCard.js
+++ frontend/src/Components/PlayerCard.js
@@ -12,7 +12,7 @@ export default function PlayerCard(props) {
 					</div>
 					<div>
 						<h2 style={{marginTop: 0, marginBottom: 0}}>{props.data.name}</h2>
-						<div style={{marginTop: '5px', color: 'rgb(90,90,90', display: props.data.name === props.steaminfo.personaname ? 'none' : 'block'}}>{props.steaminfo.personaname}</div>
+						<div style={{marginTop: '5px', color: 'rgb(90,90,90', marginBottom: '10px', display: props.data.name === props.steaminfo.personaname ? 'none' : 'block'}}>{props.steaminfo.personaname}</div>
 						<div className='teamlogo' style={{marginTop: '5px'}}><img style={{marginLeft: '0px'}} height='16px' src={`/clubs/${Teams[props.data.team].toLowerCase()}.png`} alt={props.data.name}></img> <div id='fullteamname' style={{color: 'rgb(90,90,90'}}>{props.data.team}</div></div>
 						<div style={{marginTop: '10px', color: 'rgb(90,90,90', fontSize: '0.75em'}}>{props.data.matches} partidos</div>
 						<div style={{marginTop: '5px', color: 'rgb(90,90,90', fontSize: '0.75em'}}>{`${props.data.wins} victoria${props.data.wins === 1 ? '' : 's'}`}</div>
