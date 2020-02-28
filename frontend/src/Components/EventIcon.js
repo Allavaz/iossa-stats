9f06bbdf5c36815cf534b8ca9bac06e3d43ab1c8
David Zavalla <davidzavalla@gmail.com>
2020-02-28 17:27:53 -0300

    Add link to player profile in match events

--- frontend/src/Components/EventIcon.js
+++ frontend/src/Components/EventIcon.js
@@ -3,15 +3,15 @@ import React from 'react';
 export default function EventIcon(props) {
     switch (props.event) {
         case 'GOAL':
-            return <img src='/eventicons/pelota.png' alt='Gol' height='15px' style={{paddingRight: '2.5px'}}></img>
+            return <img src='/eventicons/pelota.png' alt='Gol' height='15px' style={{marginRight: '5px'}}></img>
         case 'OWN GOAL':
-            return <img src='/eventicons/gc.png' alt='Gol en Contra' height='15px' style={{paddingRight: '2.5px'}}></img>
+            return <img src='/eventicons/gc.png' alt='Gol en Contra' height='15px' style={{marginRight: '5px'}}></img>
         case 'YELLOW CARD':
-            return <img src='/eventicons/yellowcard.png' alt='Tarjeta Amarilla' height='15px' style={{paddingRight: '2.5px'}}></img>
+            return <img src='/eventicons/yellowcard.png' alt='Tarjeta Amarilla' height='15px' style={{marginRight: '5px'}}></img>
         case 'RED CARD':
-            return <img src='/eventicons/redcard.png' alt='Tarjeta Roja' height='15px' style={{paddingRight: '2.5px'}}></img>
+            return <img src='/eventicons/redcard.png' alt='Tarjeta Roja' height='15px' style={{marginRight: '5px'}}></img>
         case 'SECOND YELLOW':
-            return <img src='/eventicons/doubleyellowcard.png' alt='Doble Amarilla' height='15px' style={{paddingRight: '2.5px'}}></img>
+            return <img src='/eventicons/doubleyellowcard.png' alt='Doble Amarilla' height='15px' style={{marginRight: '5px'}}></img>
         default:
             return null
     }
--- frontend/src/Components/MatchEvent.js
+++ frontend/src/Components/MatchEvent.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import EventIcon from './EventIcon';
+import { Link } from 'react-router-dom';
 
 export default function MatchEvent(props) {
+    console.log(props)
     if (props.side === 'home') {
         if ((props.item.team === 'home' && props.item.event !== 'OWN GOAL') || (props.item.team === 'away' && props.item.event === 'OWN GOAL')) {
             return (
                 <li key={props.index}>
                     <div className='matchevent'>
-                        <EventIcon event={props.item.event}></EventIcon>{props.item.name + ' (' + Math.round((props.item.second)/60) + "')"}
+                        <EventIcon event={props.item.event}></EventIcon><Link to={`/jugador/${props.item.player1SteamId}`}>{props.item.name + ' (' + Math.round((props.item.second)/60) + "')"}</Link>
                     </div>
                 </li>
             );
@@ -19,7 +21,7 @@ export default function MatchEvent(props) {
             return (
                 <li key={props.index}>
                     <div className='matchevent'>
-                        <EventIcon event={props.item.event}></EventIcon>{props.item.name + ' (' + Math.round((props.item.second)/60) + "')"}
+                        <EventIcon event={props.item.event}></EventIcon><Link to={`/jugador/${props.item.player1SteamId}`}>{props.item.name + ' (' + Math.round((props.item.second)/60) + "')"}</Link>
                     </div>
                 </li>
             );
