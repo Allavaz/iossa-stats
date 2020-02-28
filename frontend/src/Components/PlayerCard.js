7aa7e54f01aa81896bb6db33cd47d27cdc6f8868
David Zavalla <davidzavalla@gmail.com>
2020-02-28 00:47:48 -0300

    Show wins, draws and losses beside profile pic

--- frontend/src/Components/PlayerCard.js
+++ frontend/src/Components/PlayerCard.js
@@ -6,19 +6,22 @@ export default function PlayerCard(props) {
     return (
 		<div className='whitespace' style={{paddingBottom: '0px', marginBottom: 0, flexGrow: 1}}>
 			<div style={{display: 'flex', padding: '10px', justifyContent: 'center', flexWrap: 'wrap', alignItems: 'center'}}>
-				<div style={{display: 'flex', flexGrow: 1, maxWidth: '290px', marginBottom: '10px'}}>
+				<div style={{display: 'flex', flexGrow: 1, maxWidth: '330px', marginBottom: '10px'}}>
 					<div className='profilepicture'>
 						<img src={props.steaminfo.avatarfull} alt={props.data.name}></img>
 					</div>
 					<div>
 						<h2 style={{marginTop: 0, marginBottom: 0}}>{props.data.name}</h2>
-						<div style={{marginTop: '10px', color: 'rgb(90,90,90', display: props.data.name === props.steaminfo.personaname ? 'none' : 'block'}}>{props.steaminfo.personaname}</div>
-						<div className='teamlogo' style={{marginTop: '10px'}}><img style={{marginLeft: '0px'}} height='16px' src={`/clubs/${Teams[props.data.team].toLowerCase()}.png`} alt={props.data.name}></img> <div id='fullteamname' style={{color: 'rgb(90,90,90'}}>{props.data.team}</div></div>
-						<div style={{marginTop: '10px', color: 'rgb(90,90,90'}}>{props.data.matches} partidos</div>
+						<div style={{marginTop: '5px', color: 'rgb(90,90,90', display: props.data.name === props.steaminfo.personaname ? 'none' : 'block'}}>{props.steaminfo.personaname}</div>
+						<div className='teamlogo' style={{marginTop: '5px'}}><img style={{marginLeft: '0px'}} height='16px' src={`/clubs/${Teams[props.data.team].toLowerCase()}.png`} alt={props.data.name}></img> <div id='fullteamname' style={{color: 'rgb(90,90,90'}}>{props.data.team}</div></div>
+						<div style={{marginTop: '10px', color: 'rgb(90,90,90', fontSize: '0.75em'}}>{props.data.matches} partidos</div>
+						<div style={{marginTop: '5px', color: 'rgb(90,90,90', fontSize: '0.75em'}}>{`${props.data.wins} victoria${props.data.wins > 1 ? 's' : ''}`}</div>
+						<div style={{marginTop: '5px', color: 'rgb(90,90,90', fontSize: '0.75em'}}>{`${props.data.draws} empate${props.data.draws > 1 ? 's' : ''}`}</div>
+						<div style={{marginTop: '5px', color: 'rgb(90,90,90', fontSize: '0.75em'}}>{`${props.data.losses} derrota${props.data.wins > 1 ? 's' : ''}`}</div>
 					</div>
 				</div>
 				<RadarG data={props.data2}></RadarG>
-				<div style={{display: 'flex', justifyContent: 'space-evenly', textAlign: 'center', maxWidth: '300px', flexWrap: 'wrap'}}>
+				<div style={{display: 'flex', justifyContent: 'space-evenly', textAlign: 'center', maxWidth: '260px', flexWrap: 'wrap'}}>
 					<div className='stat'>
 						<div className='value'>{Math.round((props.data.wins/(props.data.wins+props.data.losses))*100)}%</div>
 						<div className='label'>Victorias</div>
