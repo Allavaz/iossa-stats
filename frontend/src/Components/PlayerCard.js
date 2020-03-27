20b8eda7e20de9a7a4d4b26e17b8414a6d74fa55
David Zavalla <davidzavalla@gmail.com>
2020-03-27 02:11:39 -0300

    Fix matches plural

--- frontend/src/Components/PlayerCard.js
+++ frontend/src/Components/PlayerCard.js
@@ -59,7 +59,7 @@ export default function PlayerCard(props) {
 						<div style={{display: 'inline-flex', marginTop: 0, marginBottom: 0, fontSize: '1.5em', fontWeight: 'bold'}}>{props.data.name}</div>
 						<div style={{marginTop: '5px', color: 'rgb(90,90,90)', marginBottom: '10px', display: props.data.name === props.steaminfo.personaname ? 'none' : 'block'}}>{props.steaminfo.personaname}</div>
 						<div className='teamlogo' style={{marginTop: '5px'}}><img style={{marginLeft: '0px'}} height='16px' src={`/clubs/${Teams[props.data.team].toLowerCase()}.png`} alt={props.data.name}></img> <div id='fullteamname' style={{color: 'rgb(90,90,90)'}}>{props.data.team}</div></div>
-						<div style={{marginTop: '10px', color: 'rgb(90,90,90)', fontSize: '0.75em'}}>{`${props.data.matches} partido${props.data.wins === 1 ? '' : 's'}`}</div>
+						<div style={{marginTop: '10px', color: 'rgb(90,90,90)', fontSize: '0.75em'}}>{`${props.data.matches} partido${props.data.matches === 1 ? '' : 's'}`}</div>
 						<div style={{marginTop: '5px', color: 'rgb(90,90,90)', fontSize: '0.75em'}}>{`${props.data.wins} victoria${props.data.wins === 1 ? '' : 's'}`}</div>
 						<div style={{marginTop: '5px', color: 'rgb(90,90,90)', fontSize: '0.75em'}}>{`${props.data.draws} empate${props.data.draws === 1 ? '' : 's'}`}</div>
 						<div style={{marginTop: '5px', color: 'rgb(90,90,90)', fontSize: '0.75em'}}>{`${props.data.losses} derrota${props.data.losses === 1 ? '' : 's'}`}</div>
