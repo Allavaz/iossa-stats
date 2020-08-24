c811698b3f3d069985560ad599e88c684ec0e524
Jazz <anzaldoivan98@gmail.com>
2020-08-24 02:43:32 -0300

    Limpieza

--- frontend/src/Components/Challonge.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Challonge(props) {
-    return ( <iframe title='challonge' className='whitespace' style={{padding: 0, maxWidth: 1100, marginBottom: 0}} src={"https://challonge.com/" + props.id + "/module"} width="100%" height="600" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>);
-}
