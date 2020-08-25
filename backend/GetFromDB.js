c3651a0b4ca74ebdae28997829c002ea48d2d7ac
Jazz <anzaldoivan98@gmail.com>
2020-08-25 03:43:57 -0300

    Añadida API para Stats de Equipos y Jugadores de Equipos

--- backend/GetFromDB.js
+++ backend/GetFromDB.js
@@ -1,161 +1,192 @@
-const ObjectId = require('mongodb').ObjectId;
-const MongoClient = require('mongodb').MongoClient;
-const { host, user, pw, dbname, collection } = require('./db.json');
+const ObjectId = require("mongodb").ObjectId;
+const MongoClient = require("mongodb").MongoClient;
+const { host, user, pw, dbname, collection } = require("./db.json");
 const encuser = encodeURIComponent(user);
 const encpw = encodeURIComponent(pw);
-const authMechanism = 'DEFAULT';
+const authMechanism = "DEFAULT";
 const url = `mongodb://${encuser}:${encpw}@${host}:27017/?authMechanism=${authMechanism}`;
-const positionsagg = require('./Aggregations/Positions');
-const playersagg = require('./Aggregations/Players');
-const top10goalsagg = require('./Aggregations/Top10Goals');
-const top10assistsagg = require('./Aggregations/Top10Assists');
-const top10rusticosagg = require('./Aggregations/Top10Rusticos');
-const queries = require('./Aggregations/Queries');
+const positionsagg = require("./Aggregations/Positions");
+const playersagg = require("./Aggregations/Players");
+const teamsstatsagg = require("./Aggregations/Teams");
+const teamsplayersagg = require("./Aggregations/TeamsPlayers");
+const top10goalsagg = require("./Aggregations/Top10Goals");
+const top10assistsagg = require("./Aggregations/Top10Assists");
+const top10rusticosagg = require("./Aggregations/Top10Rusticos");
+const queries = require("./Aggregations/Queries");
 
-exports.getEverything = function(res) {
-    const client = new MongoClient(url, { useNewUrlParser: true });
-    client.connect((err, client) => {
-        const db = client.db(dbname);
-        db.collection(collection).find({})
-            .toArray((err, docs) => {
-                res.json(docs);
-                client.close();
-            });
+exports.getEverything = function (res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    db.collection(collection)
+      .find({})
+      .toArray((err, docs) => {
+        res.json(docs);
+        client.close();
+      });
+  });
+};
+
+exports.getPlayers = function (id, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    db.collection(collection)
+      .aggregate(playersagg(id))
+      .toArray((err, docs) => {
+        res.json(docs);
+        client.close();
+      });
+  });
+};
+
+exports.getTop10Goals = function (id, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    db.collection(collection)
+      .aggregate(top10goalsagg(id))
+      .sort({ goals: -1, matches: 1 })
+      .limit(10)
+      .toArray((err, docs) => {
+        res.json(docs);
+        client.close();
+      });
+  });
+};
+
+exports.getTop10Assists = function (id, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    db.collection(collection)
+      .aggregate(top10assistsagg(id))
+      .sort({ assists: -1, matches: 1 })
+      .limit(10)
+      .toArray((err, docs) => {
+        res.json(docs);
+        client.close();
+      });
+  });
+};
+
+exports.getTop10Rusticos = function (id, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    db.collection(collection)
+      .aggregate(top10rusticosagg(id))
+      .sort({ redcards: -1, yellowcards: -1, fouls: 1, matches: 1 })
+      .limit(10)
+      .toArray((err, docs) => {
+        res.json(docs);
+        client.close();
+      });
+  });
+};
+
+exports.getPositions = function (id, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    db.collection(collection)
+      .aggregate(positionsagg(id))
+      .toArray((err, docs) => {
+        res.json(docs);
+        client.close();
+      });
+  });
+};
+
+exports.getMatches = function (id, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    if (id === "20") {
+      db.collection(collection)
+        .find({})
+        .sort({ fecha: -1 })
+        .project({ fecha: 1, torneo: 1, teams: 1 })
+        .limit(20)
+        .toArray((err, docs) => {
+          res.json(docs);
+          client.close();
+        });
+    } else {
+      db.collection(collection)
+        .find(queries[id])
+        .sort({ fecha: -1 })
+        .project({ fecha: 1, torneo: 1, teams: 1 })
+        .toArray((err, docs) => {
+          res.json(docs);
+          client.close();
+        });
+    }
+  });
+};
+
+exports.getMatchFromID = function (id, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    const o_id = new ObjectId(id);
+    db.collection(collection).findOne({ _id: o_id }, (err, doc) => {
+      res.json(doc);
+      client.close();
     });
-}
+  });
+};
 
-exports.getPlayers = function(id, res) {
-    const client = new MongoClient(url, { useNewUrlParser: true });
-    client.connect((err, client) => {
-        const db = client.db(dbname);
-        db.collection(collection)
-            .aggregate(playersagg(id))
-            .toArray((err, docs) => {
-                res.json(docs);
-                client.close();
-            });
-    });
-}
+exports.getPlayerFromID = function (id, torneo, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    db.collection(collection)
+      .aggregate(playersagg(torneo))
+      .match({ _id: id })
+      .toArray((err, doc) => {
+        res.json(doc);
+        client.close();
+      });
+  });
+};
 
-exports.getTop10Goals = function(id, res) {
-    const client = new MongoClient(url, { useNewUrlParser: true });
-    client.connect((err, client) => {
-        const db = client.db(dbname);
-        db.collection(collection)
-            .aggregate(top10goalsagg(id))
-            .sort({'goals': -1, 'matches': 1})
-            .limit(10)
-            .toArray((err, docs) => {
-                res.json(docs);
-                client.close();
-            });
-    });
-}
+exports.getTeamStatsFromID = function (id, torneo, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    db.collection(collection)
+      .aggregate(teamsstatsagg(torneo))
+      .match({ _id: id })
+      .toArray((err, doc) => {
+        res.json(doc);
+        client.close();
+      });
+  });
+};
 
-exports.getTop10Assists = function(id, res) {
-    const client = new MongoClient(url, { useNewUrlParser: true });
-    client.connect((err, client) => {
-        const db = client.db(dbname);
-        db.collection(collection)
-            .aggregate(top10assistsagg(id))
-            .sort({'assists': -1, 'matches': 1})
-            .limit(10)
-            .toArray((err, docs) => {
-                res.json(docs);
-                client.close();
-            });
-    });
-}
+exports.getTeamPlayersFromID = function (id, torneo, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect((err, client) => {
+    const db = client.db(dbname);
+    db.collection(collection)
+      .aggregate(teamsplayersagg(torneo))
+      .match({ _id: id })
+      .toArray((err, doc) => {
+        res.json(doc);
+        client.close();
+      });
+  });
+};
 
-exports.getTop10Rusticos = function(id, res) {
-    const client = new MongoClient(url, { useNewUrlParser: true });
-    client.connect((err, client) => {
-        const db = client.db(dbname);
-        db.collection(collection)
-            .aggregate(top10rusticosagg(id))
-            .sort({'redcards': -1, 'yellowcards': -1, 'fouls': 1, 'matches': 1})
-            .limit(10)
-            .toArray((err, docs) => {
-                res.json(docs);
-                client.close();
-            });
-    });
-}
-
-exports.getPositions = function(id, res) {
-    const client = new MongoClient(url, { useNewUrlParser: true });
-    client.connect((err, client) => {
-        const db = client.db(dbname);
-        db.collection(collection)
-            .aggregate(positionsagg(id))
-            .toArray((err, docs) => {
-                res.json(docs);
-                client.close();
-            });
-    });
-}
-
-exports.getMatches = function(id, res) {
-    const client = new MongoClient(url, { useNewUrlParser: true });
-    client.connect((err, client) => {
-        const db = client.db(dbname);
-        if (id === '20') {
-            db.collection(collection)
-            .find({})
-            .sort({'fecha': -1})
-            .project({'fecha': 1, 'torneo': 1, 'teams': 1})
-            .limit(20)
-            .toArray((err, docs) => {
-                res.json(docs);
-                client.close();
-            });
-        } else {
-            db.collection(collection)
-            .find(queries[id])
-            .sort({'fecha': -1})
-            .project({'fecha': 1, 'torneo': 1, 'teams': 1})
-            .toArray((err, docs) => {
-                res.json(docs);
-                client.close();
-            });
-        }
-    });
-}
-
-exports.getMatchFromID = function(id, res){
-    const client = new MongoClient(url, { useNewUrlParser: true })
-    client.connect((err, client) => {
-        const db = client.db(dbname);
-        const o_id = new ObjectId(id);
-        db.collection(collection)
-            .findOne({"_id": o_id}, (err, doc) => {
-                res.json(doc);
-                client.close();
-            });
-    });
-}
-
-exports.getPlayerFromID = function(id, torneo, res){
-    const client = new MongoClient(url, { useNewUrlParser: true })
-    client.connect((err, client) => {
-        const db = client.db(dbname);
-        db.collection(collection)
-            .aggregate(playersagg(torneo))
-            .match({"_id": id})
-            .toArray((err, doc) => {
-                res.json(doc);
-                client.close();
-            });
-    });
-}
-
-exports.getPlayerMatchesFromID = async function(id, res) {
-    const client = new MongoClient(url, { useNewUrlParser: true })
-    let c = await client.connect();
-    const db = c.db(dbname);
-    let matches = await db.collection(collection)
-        .find({"players.info.steam_id": id})
-        .sort({fecha: -1})
-        .toArray()
-    res.json(matches);
-}
+exports.getPlayerMatchesFromID = async function (id, res) {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  let c = await client.connect();
+  const db = c.db(dbname);
+  let matches = await db
+    .collection(collection)
+    .find({ "players.info.steam_id": id })
+    .sort({ fecha: -1 })
+    .toArray();
+  res.json(matches);
+};
--- backend/index.js
+++ backend/index.js
@@ -1,103 +1,111 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const formidable = require('formidable');
-const db = require('./PushToDB');
-const idb = require('./PushToDBios');
-const rdb = require('./GetFromDB');
-const cors = require('cors');
-const path = require('path');
-const { key } = require('./db.json');
-const steam = require('./steam');
-const serverip = require('./serverip');
+const formidable = require("formidable");
+const db = require("./PushToDB");
+const idb = require("./PushToDBios");
+const rdb = require("./GetFromDB");
+const cors = require("cors");
+const path = require("path");
+const { key } = require("./db.json");
+const steam = require("./steam");
+const serverip = require("./serverip");
 
 app.use(express.json());
 
 app.use(cors());
 
-app.post('/api/postupload', (req, res) => {
-	var form = formidable.IncomingForm();
-	form.multiples = true;
-	form.parse(req, (err, fields, files) => {
-		if (fields.pw === key) {
-			try {
-				db.pushToDB(files.upload, fields.torneo, fields.vod, res);
-			} catch(e) {
-				console.error(e);
-				res.end(e.toString());
-			}
-		} else {
-			res.end('Wrong Key');
-		}
-	});
+app.post("/api/postupload", (req, res) => {
+  var form = formidable.IncomingForm();
+  form.multiples = true;
+  form.parse(req, (err, fields, files) => {
+    if (fields.pw === key) {
+      try {
+        db.pushToDB(files.upload, fields.torneo, fields.vod, res);
+      } catch (e) {
+        console.error(e);
+        res.end(e.toString());
+      }
+    } else {
+      res.end("Wrong Key");
+    }
+  });
 });
 
-app.post('/api/postuploadios', (req, res) => {
-	let torneo = `${req.body.access_token}`;
-	let vod = "";
-	console.dir(`Received JSON from ${req.ip} with Token ID: ${torneo}`);
-	if (req.ip == serverip.serverip) {
-		try {
-			idb.pushToDBios(req.body, torneo, vod, res);
-			res.end(' -> JSON subido con exito');
-		} catch(e) {
-			console.error(e);
-			res.end(e.toString());
-		}
-	} else {
-		res.end(' -> Wrong IP');
-	}
+app.post("/api/postuploadios", (req, res) => {
+  let torneo = `${req.body.access_token}`;
+  let vod = "";
+  console.dir(`Received JSON from ${req.ip} with Token ID: ${torneo}`);
+  if (req.ip == serverip.serverip) {
+    try {
+      idb.pushToDBios(req.body, torneo, vod, res);
+      res.end(" -> JSON subido con exito");
+    } catch (e) {
+      console.error(e);
+      res.end(e.toString());
+    }
+  } else {
+    res.end(" -> Wrong IP");
+  }
 });
 
-app.get('/api/everything', (req, res) => {
-	rdb.getEverything(res);
+app.get("/api/everything", (req, res) => {
+  rdb.getEverything(res);
 });
 
-app.get('/api/players/:id', (req, res) => {
-	rdb.getPlayers(req.params.id, res);
+app.get("/api/players/:id", (req, res) => {
+  rdb.getPlayers(req.params.id, res);
 });
 
-app.get('/api/top10goals/:id', (req, res) => {
-	rdb.getTop10Goals(req.params.id, res);
+app.get("/api/top10goals/:id", (req, res) => {
+  rdb.getTop10Goals(req.params.id, res);
 });
 
-app.get('/api/top10assists/:id', (req, res) => {
-	rdb.getTop10Assists(req.params.id, res);
+app.get("/api/top10assists/:id", (req, res) => {
+  rdb.getTop10Assists(req.params.id, res);
 });
 
-app.get('/api/top10rusticos/:id', (req, res) => {
-	rdb.getTop10Rusticos(req.params.id, res);
+app.get("/api/top10rusticos/:id", (req, res) => {
+  rdb.getTop10Rusticos(req.params.id, res);
 });
 
-app.get('/api/positions/:id', (req, res) => {
-	rdb.getPositions(req.params.id, res);
+app.get("/api/positions/:id", (req, res) => {
+  rdb.getPositions(req.params.id, res);
 });
 
-app.get('/api/matches/:id', (req, res) => {
-	rdb.getMatches(req.params.id, res);
+app.get("/api/matches/:id", (req, res) => {
+  rdb.getMatches(req.params.id, res);
 });
 
-app.get('/api/match/:id', (req, res) => {
-	rdb.getMatchFromID(req.params.id, res);
+app.get("/api/match/:id", (req, res) => {
+  rdb.getMatchFromID(req.params.id, res);
 });
 
-app.get('/api/player/:id/:torneo', (req, res) => {
-	rdb.getPlayerFromID(req.params.id, req.params.torneo, res);
+app.get("/api/player/:id/:torneo", (req, res) => {
+  rdb.getPlayerFromID(req.params.id, req.params.torneo, res);
 });
 
-app.get('/api/getsteaminfo/:id', (req, res) => {
-	steam.getSteamInfo(req.params.id, res);
+app.get("/api/teams/:id/:torneo", (req, res) => {
+  rdb.getTeamStatsFromID(req.params.id, req.params.torneo, res);
 });
 
-app.get('/api/getplayermatches/:id', (req, res) => {
-	rdb.getPlayerMatchesFromID(req.params.id, res);
+app.get("/api/teamsplayers/:id/:torneo", (req, res) => {
+  rdb.getTeamPlayersFromID(req.params.id, req.params.torneo, res);
 });
 
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
-
-app.get('/*', function(req, res){
-	res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+app.get("/api/getsteaminfo/:id", (req, res) => {
+  steam.getSteamInfo(req.params.id, res);
 });
 
-app.listen(3000, "0.0.0.0", () => {
-	console.log(`Server started on port 3000`);
+app.get("/api/getplayermatches/:id", (req, res) => {
+  rdb.getPlayerMatchesFromID(req.params.id, res);
+});
+
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+});
+
+app.listen(3001, "0.0.0.0", () => {
+  console.log(`Server started on port 3001`);
 });
