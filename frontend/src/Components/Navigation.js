8a6314fe1b6a98df8f0227c44ce07d108ff295be
fetobanana <22361341+fetobanana@users.noreply.github.com>
2020-09-05 00:36:33 -0300

    Fixture T6

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -72,7 +72,7 @@ export class Navigation extends Component {
 						<Link to='/posiciones' onClick={this.hideHamburguer} id='nav-item' style={{display: this.state.hamburguer || window.innerWidth >= mobileWidth ? 'block' : 'none'}}><center>Posiciones</center></Link>
 						<Link to='/top10' onClick={this.hideHamburguer} id='nav-item' style={{display: this.state.hamburguer || window.innerWidth >= mobileWidth ? 'block' : 'none'}}><center>Rankings</center></Link>
 						<a href='https://forum.iosoccer-sa.bid/' id='nav-item' style={{display: this.state.hamburguer || window.innerWidth >= mobileWidth ? 'block' : 'none'}} target='_blank' rel='noopener noreferrer'><center>Foro</center></a>
-						<a href='https://docs.google.com/spreadsheets/d/1UeoiSeCCmpLbFOig73xwOF_NSp5GtfCiu5S8BnJvf8k/edit?usp=sharing' id='nav-item' style={{display: this.state.hamburguer || window.innerWidth >= mobileWidth ? 'block' : 'none'}} target='_blank' rel='noopener noreferrer'><center>Fixture</center></a>
+						<a href='https://docs.google.com/spreadsheets/d/1B421HesAcp-3qhGNT5qOy9tMXQovQ5SNNWOhw-MQFOA/edit?usp=sharing' id='nav-item' style={{display: this.state.hamburguer || window.innerWidth >= mobileWidth ? 'block' : 'none'}} target='_blank' rel='noopener noreferrer'><center>Fixture</center></a>
 						<div id='nav-item' onClick={this.toggleNight} style={{display: this.state.hamburguer || window.innerWidth >= mobileWidth ? 'block' : 'none'}}><center><FontAwesomeIcon icon={faLightbulb}></FontAwesomeIcon></center></div>
 					</div>
 				</div>
