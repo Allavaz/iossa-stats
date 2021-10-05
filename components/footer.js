6326f345fc4678e498ae07a96f06a95d9ea5ca28
David Zavalla <davidzavalla@gmail.com>
2021-10-05 16:49:53 -0300

    add tiktok link

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitch } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faTwitch, faInstagram, faYoutube, faDiscord, faTiktok } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
+  const socials = [
+    {
+      link: 'https://discord.me/iossudamerica',
+      icon: faDiscord
+    },
+    {
+      link: 'https://instagram.com/iosoccersa',
+      icon: faInstagram
+    },
+    {
+      link: 'https://www.youtube.com/channel/UCvr34La6BdN5wg4XHOR5nzg',
+      icon: faYoutube
+    },
+    {
+      link: 'https://www.twitch.tv/ios_sa',
+      icon: faTwitch
+    },
+    {
+      link: 'https://www.tiktok.com/@iosoccer_sudamerica',
+      icon: faTiktok
+    }
+  ];
+
   return (
     <div id='footer-wrapper'>
 			<div id='footer-text'>
@@ -13,10 +33,9 @@ export default function Footer() {
         <table className='footerTable'>
           <tbody>
             <tr>
-              <td><a href='https://discord.me/iossudamerica' target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faDiscord}></FontAwesomeIcon></a></td>
-              <td><a href='https://instagram.com/iosoccersa' target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></a></td>
-              <td><a href='https://www.youtube.com/channel/UCvr34La6BdN5wg4XHOR5nzg' target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon></a></td>
-              <td><a href='https://www.twitch.tv/ios_sa' target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faTwitch}></FontAwesomeIcon></a></td>
+              {socials.map((item, index) => (
+                <td key={index}><a href={item.link} target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={item.icon}></FontAwesomeIcon></a></td>
+              ))}
             </tr>
           </tbody>
         </table>
