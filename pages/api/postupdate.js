1e843e0a7daf1f490b2154a0d5dd1f3959fd973d
David Zavalla <davidzavalla@gmail.com>
2021-11-28 20:01:17 -0300

    fix headers trying to be set twice

--- pages/api/postupdate.js
+++ pages/api/postupdate.js
@@ -1,13 +1,16 @@
 import updateMatch from "../../lib/updateMatch";
+import createMatchCard from "../../lib/createMatchCard";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     if (req.body.password === process.env.KEY) {
       try {
-        await updateMatch(req.body.data, res);
+        let id = await updateMatch(req.body.data, res);
+        await createMatchCard(id);
+        res.json({status: "Success!"})
       } catch (e) {
         console.error(e);
-        res.end(e.toString());
+        res.json({status: "Error"})
       }
     } else {
       res.end("wrong pw");
--- pages/api/postupload.js
+++ pages/api/postupload.js
@@ -1,13 +1,16 @@
+import createMatchCard from "../../lib/createMatchCard";
 import uploadMatch from "../../lib/uploadMatch";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     if (req.body.password === process.env.KEY) {
       try {
-        await uploadMatch(req.body.data, res);
+        let id = await uploadMatch(req.body.data);
+        await createMatchCard(id);
+        res.json({status: "Success!", id: id})
       } catch (e) {
         console.error(e);
-        res.end(e.toString());
+        res.json({status: "Error"})
       }
     } else {
       res.end("wrong pw");
