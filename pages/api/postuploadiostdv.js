bc4b0d2b22f1c221fdaf589b8642ac7ae548ef5c
David Zavalla <davidzavalla@gmail.com>
2022-01-20 12:38:49 -0300

    tdv endpoint

---
+++ pages/api/postuploadiostdv.js
@@ -0,0 +1,25 @@
+import createJSON from "../../lib/createJSON";
+import uploadMatch from "../../lib/uploadMatch";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    let strArr = req.body.access_token.split("&", 2);
+    let vod = "";
+    if (strArr.length === 2) {
+      if (strArr[0] == process.env.KEY) {
+        try {
+          let doc = createJSON(req.body, strArr[1], vod);
+          let id = await uploadMatch(doc, "matchestdv");
+          res.json({ status: "Success!", id: id });
+        } catch (e) {
+          console.error(e);
+          res.json({ status: "Error" });
+        }
+      } else {
+        res.end(" -> Wrong IP");
+      }
+    }
+  } else {
+    res.end("Wrong method");
+  }
+}
