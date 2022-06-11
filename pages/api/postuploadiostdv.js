fe469fd3529b07fc48fa9c7e12cfb733e5d20cd5
David Zavalla <davidzavalla@gmail.com>
2022-06-10 21:56:28 -0300

    fix matches being autouploaded with an empty string as vod

--- pages/api/postuploadiostdv.js
+++ pages/api/postuploadiostdv.js
@@ -5,11 +5,10 @@ import uploadMatch from "../../lib/uploadMatch";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     let strArr = req.body.access_token.split("&", 2);
-    let vod = "";
     if (strArr.length === 2) {
       if (strArr[0] == process.env.KEY) {
         try {
-          let doc = createJSON(req.body, strArr[1], vod);
+          let doc = createJSON(req.body, strArr[1], null);
           let data = await uploadMatch(doc, "matchestdv");
           res.json({ status: "Success!", id: data._id.toString() });
           discordPostMatchCard(doc);
