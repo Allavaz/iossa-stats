deb48891d9204d1e9bdf24d0a920f3b935cdd73b
David Zavalla <davidzavalla@gmail.com>
2023-05-24 00:24:27 -0300

    fix vod editor not saving

--- components/vodEditor.tsx
+++ components/vodEditor.tsx
@@ -1,8 +1,11 @@
 import Button from "./commons/button";
 import Card from "./commons/card";
 import Title from "./commons/title";
+import { useState } from "react";
 
 export default function VodEditor(props) {
+  const [id, setId] = useState(props.vod === null ? "" : props.vod);
+
   return (
     <div className="flex flex-col gap-y-4">
       <Title>{props.vod ? "Editar" : "Agregar"} VOD</Title>
@@ -10,28 +13,23 @@ export default function VodEditor(props) {
         <div className="flex aspect-video w-full items-center justify-center gap-x-2">
           <input
             type="text"
+            id="inputVod"
             placeholder="ID del VOD (Ej: lQMMnMvnMLk)"
-            defaultValue={props.vod === null ? "" : props.vod}
+            defaultValue={id}
             className="w-72 rounded-lg border border-neutral-300 p-2 dark:border-neutral-700"
             autoComplete="off"
-            onChange={e =>
-              e.target.value.length
-                ? props.setEditing("vod")
-                : props.setEditing(null)
-            }
+            onChange={e => setId(e.target.value)}
           />
           <Button
             onClick={_ => {
-              props.changeVod(
-                (
-                  document.getElementById("inputVod") as HTMLInputElement
-                ).value.trim()
-              );
+              props.changeVod(id.trim());
               props.setEditing(null);
             }}
+            disabled={id.length === 0}
           >
             Guardar
           </Button>
+          <Button onClick={_ => props.setEditing(null)}>Cancelar</Button>
         </div>
       </Card>
     </div>
