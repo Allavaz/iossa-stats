ded28fe5951b6c3ec9f63ae0d76b3698a2f8ff3b
David Zavalla <davidzavalla@gmail.com>
2023-05-24 00:16:19 -0300

    convert vod component to tsx + tailwind

---
+++ components/vod.tsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import Title from "./commons/title";
+
+export default function Vod(props) {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex items-center gap-x-2">
+        <Title style={{ display: "inline", width: "fit-content" }}>
+          VIDEO DEL PARTIDO
+        </Title>
+        {props.editable ? (
+          <div className="flex gap-x-1">
+            <FontAwesomeIcon
+              icon={faEdit}
+              className="cursor-pointer"
+              onClick={_ => props.setEditing("vod")}
+            />
+            <FontAwesomeIcon
+              icon={faTrashAlt}
+              className="cursor-pointer"
+              onClick={_ => props.changeVod(null)}
+            />
+          </div>
+        ) : null}
+      </div>
+      <iframe
+        title="vod"
+        className="aspect-video w-full"
+        src={"https://www.youtube.com/embed/" + props.vod}
+        allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  );
+}
