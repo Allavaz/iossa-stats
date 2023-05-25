f1708b1f1b6027f893e9c0762f0f8b8adc649646
David Zavalla <davidzavalla@gmail.com>
2023-05-25 16:15:37 -0300

    fix header link being 100% wide

--- components/header.tsx
+++ components/header.tsx
@@ -4,11 +4,11 @@ export default function Header() {
   return (
     <div
       id="header"
-      className="mb-4 mt-12 hidden h-28 border-b-2 border-orange-300 bg-[#ff9800] dark:border-[#ff9800] dark:bg-[#a56200] dark:shadow-[0_0_100vh_#a56200] sm:flex"
+      className="mb-4 mt-12 hidden border-b-2 border-orange-300 bg-[#ff9800] dark:border-[#ff9800] dark:bg-[#a56200] dark:shadow-[0_0_100vh_#a56200] sm:flex"
     >
       <div className="m-auto w-full max-w-6xl">
         <Link href="/">
-          <a>
+          <a className="flex w-fit py-5">
             <img src="/logo-iossa.png" alt="IOSoccer Sudamérica"></img>
           </a>
         </Link>
