0565644be5ba6395f0cdc8634798bdc0bd26c530
David Zavalla <davidzavalla@gmail.com>
2023-12-23 12:50:09 -0300

    fix default indicator in result tables taking up too much width

--- components/resultWithDefault.tsx
+++ components/resultWithDefault.tsx
@@ -8,7 +8,7 @@ export default function ResultWithDefault({
   isDefault: boolean;
 }) {
   const defaultStyle =
-    "rounded border border-red-300 bg-red-500 p-1 text-xs text-white dark:border-red-600 dark:bg-red-800";
+    "rounded border border-red-300 bg-red-500 p-1 text-xs text-white dark:border-red-600 dark:bg-red-800 w-fit m-auto";
   return (
     <div className={isDefault ? defaultStyle : ""}>
       {home} - {away}
