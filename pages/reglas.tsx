caa71503ba5653ee7f5074e6f3979ab6e35d1ed4
David Zavalla <davidzavalla@gmail.com>
2023-12-22 22:49:48 -0300

    use a google docs embed for the rules

---
+++ pages/reglas.tsx
@@ -0,0 +1,8 @@
+export default function Rules() {
+  return (
+    <iframe
+      className="h-[60vh] w-full overflow-hidden rounded-lg border border-neutral-200 bg-white shadow-lg dark:border-neutral-700"
+      src="https://docs.google.com/document/d/e/2PACX-1vRGoSw89vdyVK3SdFbEOarAfcjCyz9f7SgCAyYSOgDtWTvnvAJWaRSXDWj3EB7C5f6bWENE2r6UHRmg/pub?embedded=true"
+    />
+  );
+}
