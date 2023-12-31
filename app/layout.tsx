aaa032d1ebc9242e48a05b2efb8c3f5068742988
David Zavalla <davidzavalla@gmail.com>
2023-12-31 17:25:22 -0300

    move fontawesome big icons to app router layout and remove old one

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import NextTopLoader from "nextjs-toploader";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 export const metadata: Metadata = {
   title: {
     template: "%s | IOSoccer Sudamérica",
