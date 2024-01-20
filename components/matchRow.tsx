72ac68c47d677e551bc38ace82abfd51242a9c5a
David Zavalla <davidzavalla@gmail.com>
2024-01-20 15:18:49 -0300

    fix width of match row cols being all over the place

--- components/matchRow.tsx
+++ components/matchRow.tsx
@@ -13,7 +13,10 @@ export default function MatchRow({ match }: { match: Match }) {
       href={"/partido/" + match._id}
       className="flex w-full rounded-l-lg rounded-r-lg bg-white text-sm shadow-lg transition-colors hover:bg-neutral-100 dark:bg-neutral-900 dark:hover:bg-neutral-800"
     >
-      <div className="flex w-[40%] items-center justify-end gap-x-1 rounded-l-lg border border-neutral-200 p-3 dark:border-neutral-700">
+      <div
+        className="flex w-[32%] items-center justify-end gap-x-1 rounded-l-lg border border-neutral-200 p-3 dark:border-neutral-700"
+        title={match.teams[0].teamname}
+      >
         <div className="hidden overflow-hidden overflow-ellipsis whitespace-nowrap sm:block">
           {match.teams[0].teamname}
         </div>
@@ -26,7 +29,7 @@ export default function MatchRow({ match }: { match: Match }) {
           alt={match.teams[0].teamname}
         />
       </div>
-      <div className="flex min-w-[60px] shrink-0 items-center justify-center border-y border-r p-2 dark:border-neutral-700">
+      <div className="flex w-[60px] shrink-0 items-center justify-center border-y border-r p-2 dark:border-neutral-700">
         <div className="flex flex-col items-center gap-y-1">
           <ResultWithDefault
             home={match.teams[0].score}
@@ -35,7 +38,10 @@ export default function MatchRow({ match }: { match: Match }) {
           />
         </div>
       </div>
-      <div className="flex w-[40%] items-center gap-x-1 border-y border-r p-3 dark:border-neutral-700">
+      <div
+        className="flex w-[32%] items-center gap-x-1 border-y border-r p-3 dark:border-neutral-700"
+        title={match.teams[1].teamname}
+      >
         <img
           className="h-6"
           src={getTeamLogo(match.teams[1].teamname)}
@@ -48,7 +54,10 @@ export default function MatchRow({ match }: { match: Match }) {
           {getTeamShortname(match.teams[1].teamname)}
         </div>
       </div>
-      <div className="flex w-[20%] items-center justify-center gap-x-1 rounded-r-lg border-y border-r p-3 dark:border-neutral-700 sm:w-[30%]">
+      <div
+        className="flex w-[20%] items-center justify-center gap-x-1 rounded-r-lg border-y border-r p-3 dark:border-neutral-700 sm:w-[28%]"
+        title={match.torneo}
+      >
         <img
           className="h-6"
           src={getTournamentIcon(match.torneo)}
