a1bab1f9308c531ea2b6d8940430f0b37bb51b8f
David Zavalla <davidzavalla@gmail.com>
2024-06-17 10:45:40 -0300

    handle missing/broken json payload

--- app/api/postuploadios/route.ts
+++ app/api/postuploadios/route.ts
@@ -4,22 +4,22 @@ import createMatchCard from "../../../lib/createMatchCard";
 import discordPostMatch from "../../../lib/discordPostMatch";
 
 export async function POST(request: Request) {
-  const body = await request.json();
-  let strArr = body.access_token?.split("&", 2);
+  let body: any;
+  try {
+    body = await request.json();
+  } catch (e) {
+    return new Response("Error parsing JSON body");
+  }
+  const strArr = body.access_token?.split("&", 2);
   if (strArr?.length === 2) {
     if (strArr[0] == process.env.KEY) {
-      try {
-        let doc = createJSON(body, strArr[1], null);
-        let data = await uploadMatch(doc);
-        createMatchCard({
-          ...data,
-          fecha: data.fecha.toISOString()
-        }).then(() => discordPostMatch(data));
-        return new Response("Success!");
-      } catch (e) {
-        console.error(e);
-        return new Response("Error");
-      }
+      const doc = createJSON(body, strArr[1], null);
+      const data = await uploadMatch(doc);
+      createMatchCard({
+        ...data,
+        fecha: data.fecha.toISOString()
+      }).then(() => discordPostMatch(data));
+      return new Response("Success!");
     } else {
       return new Response("Wrong password");
     }
