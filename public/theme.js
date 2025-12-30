660e1cd00b1193db277c1c76a0248d458aa85f32
David Zavalla <davidzavalla@gmail.com>
2025-12-30 16:07:32 +1300

    that seemed to fix it. delete theme.js

--- public/theme.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-  document.documentElement.classList.toggle(
-    "dark",
-    localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-  );
-})();
