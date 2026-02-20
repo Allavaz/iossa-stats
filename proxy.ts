9075c352d980f2ee51f1dd58e0bcaff7a54a601d
David Zavalla <davidzavalla@gmail.com>
2026-02-21 11:25:33 +1300

    middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   await auth();
 
   // Create response
