ac564debd016e447dc8e35a89f693c32b0107b06
David Zavalla <davidzavalla@gmail.com>
2026-04-27 11:56:50 +1200

    fix rules iframe height on mobile and desktop to avoid double scrolling

--- app/reglas/page.tsx
+++ app/reglas/page.tsx
@@ -5,7 +5,7 @@ export const metadata = {
 export default async function Rules() {
   return (
     <iframe
-      className="h-[60vh] w-full overflow-hidden rounded-lg border border-neutral-200 bg-white shadow-lg dark:border-neutral-700"
+      className="h-[70vh] w-full overflow-hidden rounded-lg border border-neutral-200 bg-white shadow-lg dark:border-neutral-700 sm:h-[58vh]"
       src="https://docs.google.com/document/d/e/2PACX-1vRGoSw89vdyVK3SdFbEOarAfcjCyz9f7SgCAyYSOgDtWTvnvAJWaRSXDWj3EB7C5f6bWENE2r6UHRmg/pub?embedded=true"
     />
   );
